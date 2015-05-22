562212a874158cde05d24ed7be4f9a820f98cfff
peruvs <peruvs@gmail.com>
2015-05-22 16:03:59 -0600

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -15,13 +15,22 @@ for x in range(0, 300, 10):
     name = root.cssselect('a.biz-name')
     address = root.cssselect('address')
     phone = root.cssselect('span.biz-phone')
+    cat = root.cssselect('span.category-str-list')
     
     for i in range(len(name)):
+        my_cat = cat[i]
+        my_cats = my_cat.cssselect('a')
+        category = ""
+        for j in range(len(my_cats)):
+            category += ", " + my_cats[j].text.encode('utf-8').decode('utf-8').strip()
+            
         scraperwiki.sqlite.save(unique_keys=["pk"], data={
                 'pk': index,
                 'name': name[i].text.encode('utf-8').decode('utf-8').strip(),
                 'address': address[i].text.encode('utf-8').decode('utf-8').strip(),
                 'phone': phone[i].text.encode('utf-8').decode('utf-8').strip(),
+                'category': category,
+                'url': name[i].attrib['href'].strip()
             })
         index += 1
         #print name[i].text.encode('utf8')
