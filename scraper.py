567e22f4f37a80dedcacf8981ecf3f733f6aac9d
peruvs <peruvs@gmail.com>
2015-05-22 15:48:37 -0600

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -4,25 +4,28 @@ import re
 import time
 
 yelpurl = "http://www.yelp.ca/search?cflt=gyms&find_loc=Calgary%2C+AB#find_desc&start="
-
-html = scraperwiki.scrape(yelpurl)
-time.sleep(.2)
-root = lxml.html.fromstring(html)
-
-name = root.cssselect('a.biz-name')
-address = root.cssselect('address')
-phone = root.cssselect('span.biz-phone')
-
 index = 1
-for i in range(len(name)):
-    scraperwiki.sqlite.save(unique_keys=["pk"], data={
-            'pk': index++,
-            'name': name[i].text.encode('utf8'),
-            'address': address[i].text.encode('utf8'),
-            'phone': phone[i].text.encode('utf8'),
-        })
-    #print name[i].text.encode('utf8')
-   # print address[i].text.encode('utf8')
-   # print phone[i].text.encode('utf8')
+
+for x in range(0, 205, 10):
+    print x
+    html = scraperwiki.scrape(yelpurl + x)
+    time.sleep(.2)
+    root = lxml.html.fromstring(html)
+    
+    name = root.cssselect('a.biz-name')
+    address = root.cssselect('address')
+    phone = root.cssselect('span.biz-phone')
+    
+    for i in range(len(name)):
+        scraperwiki.sqlite.save(unique_keys=["pk"], data={
+                'pk': index,
+                'name': name[i].text.encode('utf8'),
+                'address': address[i].text.encode('utf8'),
+                'phone': phone[i].text.encode('utf8'),
+            })
+        index += 1
+        #print name[i].text.encode('utf8')
+       # print address[i].text.encode('utf8')
+       # print phone[i].text.encode('utf8')
 
     
